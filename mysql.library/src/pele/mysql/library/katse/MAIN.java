494b93bdbe3b6c14c2e4a31a21eeafd9536cb373
pele <pelleproosa@gmail.com>
2014-02-21 21:02:23 +0200

    mysql algus

---
+++ mysql.library/src/pele/mysql/library/katse/MAIN.java
@@ -0,0 +1,15 @@
+package pele.mysql.library.katse;
+
+//import pele.mysql.library.katse.*;;
+public class MAIN {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		//MySQLAccess dao = new MySQLAccess();dao.readDataBase();
+		teine_example.start(null);
+		
+
+	}
+
+}
+
